7f7302faa1d250119c63b501973485821e0884e8
Abdul Wahab Qureshi <its.inevitable@hotmail.com>
2017-12-19 16:33:19 +0000

    Convenience methods separated as their scope is usually wider than just data mods

--- src/BaseProvider.php
+++ src/BaseProvider.php
@@ -251,42 +251,6 @@ abstract class BaseProvider implements APIDecoratorInterface
     }
 
     /**
-     * Convenience method to get a required index value out of array.
-     *
-     * @param array $data The data to check.
-     * @param string $key The index to look for.
-     *
-     * @return mixed Whatever the data index contains.
-     */
-    public static function getRequiredData(array $data, $key)
-    {
-        if (! array_key_exists($key, $data)) {
-            throw new Exception("Expect to have key '$key' provided.");
-        }
-
-        return $data[$key];
-    }
-
-    /**
-     * Convenience method to get an optional index value out of array, if it does not exist will
-     * return the default value.
-     *
-     * @param array $data The data to check.
-     * @param string $key The index to look for.
-     * @param string $default The value to return if the key index is not defined.
-     *
-     * @return mixed Whatever the data index contains.
-     */
-    public static function getOptionalData(array $data, $key, $default = null)
-    {
-        if (! array_key_exists($key, $data)) {
-            return $default;
-        }
-
-        return $data[$key];
-    }
-
-    /**
      * Get the field mapping for a column.
      *
      * @param string $key The key to get the mapping for.
